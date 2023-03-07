da4c7d9628f356a0499fa41910931bd7519863ca
arplaaay <artemb0607@gmail.com>
2023-03-08 01:23:32 +0200

    Added routes

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,70 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("./models/contacts.json");
 
-const getContactById = async (contactId) => {}
+async function updateContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 1));
+}
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const findContactId = contacts.find((contact) => contact.id === contactId);
 
-const updateContact = async (contactId, body) => {}
+  return findContactId;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const findContactIndex = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (findContactIndex > -1) {
+    const removeContact = contacts.splice(findContactIndex, 1);
+
+    updateContacts(contacts);
+    return removeContact;
+  } else {
+    console.log("Enter valid value!");
+  }
+};
+
+async function addContact(body) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...body,
+  };
+
+  contacts.push(newContact);
+  updateContacts(contacts);
+  return newContact;
+}
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+
+  const findContactIndex = contacts.findIndex(({ id }) => id === contactId);
+
+  if (findContactIndex > -1) {
+    const updContact = contacts[findContactIndex];
+
+    contacts[findContactIndex] = {
+      ...updContact,
+      ...body,
+    };
+
+    updateContacts(contacts);
+    return contacts[findContactIndex];
+  }
+
+  return null;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +72,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
